246a1baeebf6a42bf110e32549f4e4d082a3782e
Ruyer Avila <Ruyer Avila@10.15.10.41>
2017-05-19 02:26:21 -0400

    EJERCICIO 2 : ENCONTRAR EL NUMERO MENOR DE 2 NUMEROS

--- Ejercicio2.php
+++ Ejercicio2.php
@@ -3,10 +3,10 @@
 $a = 15;
 $b = 8;
 if ($a > $b) {
-    $menor = 'b';
+    $menor = 'b = '.$b;
 }else{ 
-     $menor = 'a';
+     $menor = 'a = '.$a;
 }
-
-echo 'la menor es: ' . $menor . '<br />';
+echo 'Se eligio los numeros $a=15 y $b=8. <br />';
+echo 'Se Calculo que El Numero menor seria la: ' . $menor .'<br />';
 ?>
