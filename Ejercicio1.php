be7334af5633b9e1ef745ee898d0156cfd9d1ef6
Luiyi Rodriguez Torres <lert.04.95@gmail.com>
2017-05-19 12:36:47 -0500

    EJERCICIO 1 : ENCONTRAR EL NUMERO MAYOR DE 2 NUMEROS EJERCICIO 2 : ENCONTRAR EL NUMERO MENOR DE 2 NUMEROS

---
+++ Ejercicio1.php
@@ -0,0 +1,19 @@
+<?php
+echo 'EJERCICIO 2 <br />';
+echo '<br />';
+function numeromayor(){
+    $numero1=2;
+    $numero2=5;
+    if ($numero1>$numero2) {
+        echo 'Se compararon 2 numeros: '.$numero1.' y '.$numero2.'<br/>';
+        echo'El numero mayor es: '.$numero1;
+    }else{
+        echo 'Se compararon 2 numeros: '.$numero1.' y '.$numero2.'<br/>';
+        echo'El numero mayor es: '.$numero2;
+    }
+    
+   
+    
+}
+ numeromayor();
+?>
